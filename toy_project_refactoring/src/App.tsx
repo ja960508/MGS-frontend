9a5fb03c0b50564cf246db41bab764374981d224
jooam <lord0325@naver.com>
2022-06-03 21:59:49 +0900

    feat: Toy Project Refactoring, Set Loading

--- toy_project_refactoring/src/App.tsx
+++ toy_project_refactoring/src/App.tsx
@@ -1,14 +1,28 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import Account from "./components/Account/Account";
+import Loading from "./components/commons/Loading";
 
 function App({ service }: { service: any }) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleLoading = (flag: boolean) => {
+    setLoading(flag);
+  };
+
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Account service={service} />} />
-        <Route path='/:id' element={<Account service={service} />} />
+        <Route
+          path='/'
+          element={<Account handleLoading={handleLoading} service={service} />}
+        />
+        <Route
+          path='/:id'
+          element={<Account handleLoading={handleLoading} service={service} />}
+        />
       </Routes>
+      {loading && <Loading />}
     </div>
   );
 }
